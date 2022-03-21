1ccc897f222e4162556245476196c8b3e33fb1e2
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-03-21 17:01:23 +0100

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -12,5 +12,5 @@ weatherDescription = weatherData['weather'][0]['description']
 temp = weatherData['main']['temp_max']
 
 st.text(weatherDescription)
-st.metric(label='temperature (°C)', value= temp)
+st.metric(label=' max temperature (°C)', value= temp)
 
