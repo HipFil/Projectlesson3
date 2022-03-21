fc7054509f7cc78a1da3c7cbe1faced76e066363
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-03-21 16:21:47 +0100

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import json, requests 
 
 APIkey = '0a127aa062bc472c6fc8866ac02bf99c'
-location = st.text_input('give me a city')
+location = st.radio("choose a city",('Cuneo', 'Amsterdam', 'Bolzano'))
 
 url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey 
 response = requests.get(url)
