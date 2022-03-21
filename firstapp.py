822d22c60ecf979905795a4fea74b185e1aea090
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-03-21 16:29:34 +0100

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -5,7 +5,7 @@ st.header('Weather Forecast')
 APIkey = '0a127aa062bc472c6fc8866ac02bf99c'
 location = st.radio("pick a city",('Cuneo', 'Amsterdam', 'London'))
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey 
+url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey + '&units=metric' 
 response = requests.get(url)
 weatherData = json.loads(response.text)
 weatherDescription = weatherData['weather'][0]['description']
