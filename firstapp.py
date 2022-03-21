153489f5bdbb538e917b2d9f1be764d6214516e8
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-03-21 16:12:18 +0100

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -4,7 +4,7 @@ import json, requests
 APIkey = '0a127aa062bc472c6fc8866ac02bf99c'
 location = 'london'
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey + &units=metric'
+url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey + '&units=' + metric
 response = requests.get(url)
 weatherData = json.loads(response.text)
 
