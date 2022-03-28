e2c50b51ea46155b8fa85799c4b062ead6f0ecf2
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-03-28 16:49:12 +0200

    Update datamuse.py

--- datamuse.py
+++ datamuse.py
@@ -4,15 +4,5 @@ import json, request
 st.header('Exercise with Datamuse')
 selection = st.selectbox('what do you want to know?', ('means like...', 'sounds like...', 'antonyms', 'synonims'))
 
-keyword = st.text_input('give me word')
-
-if selection == 'means like':
-    url= 'https://api.datamuse.com/words?ml=' + keyword 
-elif selection == 'sounds like':
-    url= 'https://api.datamuse.com/words?sl=' + keyword 
-elif selection == 'antonyms':
-    url= 'https://api.datamuse.com/words?rel_ant=' + keyword 
-else selection == 'synonims':
-    url= 'https://api.datamuse.com/words?rel_syn=' + keyword 
 
     
