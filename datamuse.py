44853edb75d3cbabf1c928370549d07a631bbc01
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-03-28 16:15:29 +0200

    Update datamuse.py

--- datamuse.py
+++ datamuse.py
@@ -2,3 +2,4 @@ import streamlit as st
 import json, request
 
 st.header('Exercise with Datamuse')
+st.selectbox('what do you want to know?', ('means like...', 'sounds like...', 'antonyms', 'synonims')
