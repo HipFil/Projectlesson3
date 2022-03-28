feb1742770028b6147e87cb4f1263fdbe1af177a
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-03-28 16:58:12 +0200

    Update datamuse.py

--- datamuse.py
+++ datamuse.py
@@ -1,8 +1,20 @@
 import streamlit as st
-import json, request
+import json, requests
 
 st.header('Exercise with Datamuse')
 selection = st.selectbox('what do you want to know?', ('means like...', 'sounds like...', 'antonyms', 'synonims'))
 
+keyword = st.text_input('give me word')
 
-    
+if selection == 'means like':
+    url= 'https://api.datamuse.com/words?ml=' + keyword 
+elif selection == 'sounds like':
+    url= 'https://api.datamuse.com/words?sl=' + keyword 
+elif selection == 'antonyms':
+    url= 'https://api.datamuse.com/words?rel_ant=' + keyword 
+else selection == 'synonims':
+    url= 'https://api.datamuse.com/words?rel_syn=' + keyword
+
+response = requests.get(url)
+dataFromDatamuse = json.loads(response.text)
+pprint(dataFromDatamuse[0:4])
