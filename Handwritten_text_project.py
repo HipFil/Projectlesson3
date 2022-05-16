00760e45fbe977656e1eba048f00fda26640086a
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-05-16 16:54:08 +0200

    Update Handwritten_text_project.py

--- Handwritten_text_project.py
+++ Handwritten_text_project.py
@@ -22,29 +22,33 @@ endpoint = credential['Endpoint']
 
 cv_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(API_key))
 
-'https://www.opensourceforu.com/wp-content/uploads/2016/09/Figure-1-Sample-Page-1.jpg'
+uploaded_files = st.file_uploader('Upload a text image', type='jpg', accept_multiple_files=True, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False)
+for file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
+    
+    lan = input('give me a 2 letter word of your file langauge: ')
 
-lan = input('give me a 2 letter word of your file langauge: ')
-#image_url = 'https://www.opensourceforu.com/wp-content/uploads/2016/09/Figure-1-Sample-Page-1.jpg'
-local_file = 'WhatsApp Image 2022-05-11 at 15.21.41.jpeg'
-response = cv_client.read(url = image_url, Language= lan, raw=True)
-#response = cv_client.read_in_stream(open(local_file, 'rb'), Language= lan, raw=True)
-operationLocation = response.headers['Operation-Location']
-print(operationLocation)
-operation_id = operationLocation.split('/')[-1]
-time.sleep(5)
-print(operation_id)
-result = cv_client.get_read_result(operation_id)
+    local_file = file
+    #response = cv_client.read(url = image_url, Language= lan, raw=True)
+    response = cv_client.read_in_stream(open(local_file, 'rb'), Language= lan, raw=True)
+    operationLocation = response.headers['Operation-Location']
+    #print(operationLocation)
+    operation_id = operationLocation.split('/')[-1]
+    time.sleep(5)
+    print(operation_id) 
+    result = cv_client.get_read_result(operation_id)
 
-print(result)
-print(result.status)
-#print(result.analyze_result)
+    #print(result)
+    #print(result.status)
+    #print(result.analyze_result)
 
-if result.status == OperationStatusCodes.succeeded:
-    read_results = result.analyze_result.read_results
-    for analyze_result in read_results:
-        for line in analyze_result.lines:
-            print(line.text)
+    if result.status == OperationStatusCodes.succeeded:
+        read_results = result.analyze_result.read_results
+        for analyze_result in read_results:
+            for line in analyze_result.lines:
+                print(line.text)
             
 def HWTEXT():
      for line in analyze_result.lines:
