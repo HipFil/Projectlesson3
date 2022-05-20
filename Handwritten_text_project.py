2983784170f7534796a27042ce60448f58a6be00
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-05-20 11:09:23 +0200

    Update Handwritten_text_project.py

--- Handwritten_text_project.py
+++ Handwritten_text_project.py
@@ -27,29 +27,29 @@ for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     file_name = st.write("filename:", uploaded_file.name)
     #st.write(bytes_data)
-    image = Image.open(file_name)
+    image = Image.open(uploaded_file)
     st.image(image)
     
-    sorce_lan = st.text_input('give me a 2 letter word of your file langauge: ')
+sorce_lan = st.text_input('give me a 2 letter word of your file langauge: ')
 
-    local_file = uploaded_file
+local_file = uploaded_file
     #response = cv_client.read(url = image_url, Language= lan, raw=True)
-    response = cv_client.read_in_stream(open(local_file, 'rb'), Language= source_lan, raw=True)
-    operationLocation = response.headers['Operation-Location']
+response = cv_client.read_in_stream(open(local_file, 'rb'), Language= source_lan, raw=True)
+operationLocation = response.headers['Operation-Location']
     #print(operationLocation)
-    operation_id = operationLocation.split('/')[-1]
+operation_id = operationLocation.split('/')[-1]
     time.sleep(5)
     #print(operation_id) 
-    result = cv_client.get_read_result(operation_id)
+result = cv_client.get_read_result(operation_id)
 
     #print(result)
     #print(result.status)
     #print(result.analyze_result)
 
-    if result.status == OperationStatusCodes.succeeded:
-        read_results = result.analyze_result.read_results
-        for analyze_result in read_results:
-            for line in analyze_result.lines:
-                st.write(line.text)
+if result.status == OperationStatusCodes.succeeded:
+    read_results = result.analyze_result.read_results
+    for analyze_result in read_results:
+        for line in analyze_result.lines:
+            st.write(line.text)
             
 
