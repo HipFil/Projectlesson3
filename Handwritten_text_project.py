a8454b01cd61cd5e027780a621193dfbf405cea7
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-05-21 14:56:45 +0200

    Update Handwritten_text_project.py

--- Handwritten_text_project.py
+++ Handwritten_text_project.py
@@ -22,59 +22,63 @@ endpoint = credential['Endpoint']
 
 cv_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(API_key))
 
-uploaded_files = st.file_uploader('Upload a text image', type='jpg', accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    file_name = st.write("filename:", uploaded_file.name)
-    #st.write(bytes_data)
-    image = Image.open(uploaded_file)
-    st.image(image)
-    
-sorce_lan = st.multiselect('give me a 2 letter word of your file langauge: ', ["BG",
-"CS",
-"DA",
-"DE",
-"EL"                                                                              ,
-"EN",
-"ES",
-"ET",
-"FI",
-"FR",
-"HU",
-"ID",
-"IT",
-"JA",
-"LT",
-"LV",
-"NL",
-"PL",
-"PT",
-"RO",
-"RU",
-"SK",
-"SL",
-"SV",
-"TR",
-"ZH"])
+uploaded_file = st.file_uploader('Upload a text image', type='jpg', accept_multiple_files=False)
 
-local_file = uploaded_file
+if uploded_file == True:
+    pass
+else:
+    for uploaded_file in uploaded_files:
+        bytes_data = uploaded_file.read()
+        file_name = st.write("filename:", uploaded_file.name)
+    #st.write(bytes_data)
+        image = Image.open(uploaded_file)
+        st.image(image)
+    
+        sorce_lan = st.multiselect('give me a 2 letter word of your file langauge: ', ["BG",
+        "CS",
+        "DA",
+        "DE",
+        "EL"                                                                              ,
+        "EN",
+        "ES",
+        "ET",
+        "FI",
+        "FR",
+        "HU",
+        "ID",
+        "IT",
+        "JA",
+        "LT",
+        "LV",
+        "NL",
+        "PL",
+        "PT",
+        "RO",
+        "RU",
+        "SK",
+        "SL",
+        "SV",
+        "TR",
+        "ZH"])
+
+    local_file = uploaded_file
     #response = cv_client.read(url = image_url, Language= lan, raw=True)
-response = cv_client.read_in_stream(open(local_file, 'rb'), Language= source_lan, raw=True)
-operationLocation = response.headers['Operation-Location']
+    response = cv_client.read_in_stream(open(local_file, 'rb'), Language= source_lan, raw=True)
+    operationLocation = response.headers['Operation-Location']
     #print(operationLocation)
-operation_id = operationLocation.split('/')[-1]
-time.sleep(5)
+    operation_id = operationLocation.split('/')[-1]
+    time.sleep(5)
     #print(operation_id) 
-result = cv_client.get_read_result(operation_id)
+    result = cv_client.get_read_result(operation_id)
 
     #print(result)
     #print(result.status)
     #print(result.analyze_result)
 
-if result.status == OperationStatusCodes.succeeded:
-    read_results = result.analyze_result.read_results
-    for analyze_result in read_results:
-        for line in analyze_result.lines:
-            st.write(line.text)
+    if result.status == OperationStatusCodes.succeeded:
+        read_results = result.analyze_result.read_results
+        for analyze_result in read_results:
+            for line in analyze_result.lines:
+                st.write(line.text)
             
 
