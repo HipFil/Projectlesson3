7ef1ae2f996e8f1443408ca3be22218d09e13d2c
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-05-21 18:06:12 +0200

    Update Handwritten_text_project.py

--- Handwritten_text_project.py
+++ Handwritten_text_project.py
@@ -63,7 +63,7 @@ if uploaded_files is not None:
         
         
     
-        response = cv_client.read_in_stream(open(uploaded_file, "rb"), Language= source_lan, raw=True)
+        response = cv_client.read_in_stream(open(uploaded_files, "rb"), Language= source_lan, raw=True)
         operationLocation = read_response.headers['Operation-Location']
     
         operation_id = operationLocation.split('/')[-1]
