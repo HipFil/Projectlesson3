37750d16701f58266a76f079e7ffe78f6f4db068
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-05-23 20:10:47 +0200

    Update Handwritten_text_project.py

--- Handwritten_text_project.py
+++ Handwritten_text_project.py
@@ -75,12 +75,12 @@ if result.status == OperationStatusCodes.succeeded:
             str_=re.findall("[a-zA-Z,.]+", line_text)
             updated_docx=(" ".join(str_))
             st.write(updated_docx)
-            new_doc = TextBlob(updated_docx)
+            #new_doc = TextBlob(updated_docx)
       
-            result1 = result1 + " " + str(new_doc.correct())
+            #result1 = result1 + " " + str(new_doc.correct())
 
 
-st.write(result1)
+#st.write(result1)
 
 
 uploaded_file = st.file_uploader('Upload a text image', type='jpg')
