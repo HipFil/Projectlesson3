dee8a560ad37b8669b2aa7e90c42d2fe38b67981
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-05-24 11:45:06 +0200

    Update project_2.py

--- project_2.py
+++ project_2.py
@@ -55,35 +55,35 @@ source_lan = st.multiselect('give me a 2 letter word of your file langauge: ', [
 
 
 image_url = st.text_input('url: https://www.opensourceforu.com/wp-content/uploads/2016/09/Figure-1-Sample-Page-1.jpg')
-response = cv_client.read(url = image_url, Language= source_lan, raw=True)
-operationLocation = response.headers['Operation-Location']
+
+if image_url is not None:
+
+    response = cv_client.read(url = image_url, Language= source_lan, raw=True)
+    operationLocation = response.headers['Operation-Location']
     
-operation_id = operationLocation.split('/')[-1]
-time.sleep(5)
+    operation_id = operationLocation.split('/')[-1]
+    time.sleep(5)
     
-result = cv_client.get_read_result(operation_id)
+    result = cv_client.get_read_result(operation_id)
 
-    #st.write(result)
-#st.write(result.status)
-#st.write(result.analyze_result)
 
-col1, col2 = st.columns(2)
+    col1, col2 = st.columns(2)
 
-if result.status == OperationStatusCodes.succeeded:
-    read_results = result.analyze_result.read_results
-    for analyze_result in read_results:
-        for line in analyze_result.lines:
-            line_text = line.text
-            #st.write(line_text)
-            str_=re.findall("[a-zA-Z,.]+", line_text)
-            updated_docx=(" ".join(str_))
+    if result.status == OperationStatusCodes.succeeded:
+        read_results = result.analyze_result.read_results
+        for analyze_result in read_results:
+            for line in analyze_result.lines:
+                line_text = line.text
+                #st.write(line_text)
+                str_=re.findall("[a-zA-Z,.]+", line_text)
+                updated_docx=(" ".join(str_))
             
-            with col1:
-                st.header('the original line is:')
-                st.write(updated_docx)
+                with col1:
+                    st.subheader('the original line is:')
+                    st.write(updated_docx)
     
-            new_doc = TextBlob(updated_docx)
-            result = str(new_doc.correct())
-            with col2:
-                st.header('the corrected line is:')
-                st.write(result)
+                new_doc = TextBlob(updated_docx)
+                result = str(new_doc.correct())
+                with col2:
+                    st.subheader('the corrected line is:')
+                    st.write(result)
