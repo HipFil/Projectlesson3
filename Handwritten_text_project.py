6f5f98818f00ec309148b33540011d413efe8ed5
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-05-25 17:04:35 +0200

    Update Handwritten_text_project.py

--- Handwritten_text_project.py
+++ Handwritten_text_project.py
@@ -26,6 +26,11 @@ endpoint = credential['Endpoint']
 
 cv_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(API_key))
 
+st.title('HW-Reader')
+         
+st.write('''Hello user! I am HW-Reader, a simple app to read, correct and translate your handwritten texts and notes.''')
+st.write('''Before you start, let me briefly explain how I work: 1. select the language of your text file 2. upload a photo of your text 3. download the corrected text if needed 4. select the language and translate the text.'''  
+
 source_lan = st.multiselect('give me a 2 letter word of your file langauge: ', ["BG",
         "CS",
         "DA",
@@ -52,7 +57,6 @@ source_lan = st.multiselect('give me a 2 letter word of your file langauge: ', [
         "SV",
         "TR",
         "ZH"])
-
 
 uploaded_file = st.file_uploader('Upload a text image', type= ['jpg', 'jpeg'])
 #image_url = st.text_input('url: https://www.opensourceforu.com/wp-content/uploads/2016/09/Figure-1-Sample-Page-1.jpg')
