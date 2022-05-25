d43e8b78d0d2b42b00bfc3066c801bad224af534
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-05-25 17:48:53 +0200

    Update Handwritten_text_project.py

--- Handwritten_text_project.py
+++ Handwritten_text_project.py
@@ -71,7 +71,7 @@ if uploaded_file is not None:
     st.image(image)
     #response = cv_client.read(url = image_url, Language= source_lan, raw=True)
     response = cv_client.read_in_stream(open('handwriting1.jpg', "rb"), Language= source_lan, raw=True)
-    
+    operationLocation = response.headers['Operation-Location']
     operation_id = operationLocation.split('/')[-1]
     time.sleep(1)
     
