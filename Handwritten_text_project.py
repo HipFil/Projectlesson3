0d6dcce439909f46f022068d2aef1cdd333fb647
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-05-25 10:05:58 +0200

    Update Handwritten_text_project.py

--- Handwritten_text_project.py
+++ Handwritten_text_project.py
@@ -54,14 +54,14 @@ source_lan = st.multiselect('give me a 2 letter word of your file langauge: ', [
         "ZH"])
 
 
-uploaded_file = st.file_uploader('Upload a text image')
+uploaded_file = st.file_uploader('Upload a text image', type= None)
 
 
 if uploaded_file is not None:
     #bytes_data = uploaded_file.read()
     image = Image.open(uploaded_file)
     st.image(image)
-    response = cv_client.read_in_stream(open(image, "rb"), raw=True)
+    response = cv_client.read_in_stream(open(uploaded_file, "rb"), Language = source_lan, raw=True)
     operationLocation = response.headers['Operation-Location']
     
     operation_id = operationLocation.split('/')[-1]
