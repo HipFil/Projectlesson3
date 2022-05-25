1fb896074e5c3f0f69f9bf044c8129b171b04491
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-05-25 17:07:53 +0200

    Update Handwritten_text_project.py

--- Handwritten_text_project.py
+++ Handwritten_text_project.py
@@ -26,7 +26,7 @@ endpoint = credential['Endpoint']
 
 cv_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(API_key))
 
-#st.title('HW-Reader')
+st.title('HW-Reader')
          
 #st.write('''Hello user! I am HW-Reader, a simple app to read, correct and translate your handwritten texts and notes.''')
 #st.write('''Before you start, let me briefly explain how I work: 1. select the language of your text file 2. upload a photo of your text 3. download the corrected text if needed 4. select the language and translate the text.'''  
