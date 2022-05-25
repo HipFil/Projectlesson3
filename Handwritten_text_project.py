a77f26a7321968760b5ce6e0a702eac383b1d8e0
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-05-25 17:10:01 +0200

    Update Handwritten_text_project.py

--- Handwritten_text_project.py
+++ Handwritten_text_project.py
@@ -29,7 +29,7 @@ cv_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(API_key)
 st.title('HW-Reader')
          
 st.write('''Hello user! I am HW-Reader, a simple app to read, correct and translate your handwritten texts and notes.''')
-st.write('''Before you start, let me briefly explain how I work: 1. select the language of your text file 2. upload a photo of your text 3. download the corrected text if needed 4. select the language and translate the text.'''  
+st.write('''Before you start, let me briefly explain how I work: 1. select the language of your text file 2. upload a photo of your text 3. download the corrected text if needed 4. select the language and translate the text.''')  
 
 source_lan = st.multiselect('give me a 2 letter word of your file language: ', ["BG",
         "CS",
