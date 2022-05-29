612bbeb4646a4690db71b0dc5c80061c050fa0c5
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-05-29 12:11:20 +0200

    Update Handwritten_text_project.py

--- Handwritten_text_project.py
+++ Handwritten_text_project.py
@@ -119,7 +119,7 @@ with col2:
     audio_file = open('file_name.mp3', "rb")
     st.audio(data=audio_file, format="audio/mp3", start_time=0
 
-    st.download_button('Download corrected text', result2)
+st.download_button('Download corrected text', result2)
 
 choice = st.radio("'Do you want to translate the text?", ("yes", "no"))
 deepl_api = st.secrets['deeplAPI']
