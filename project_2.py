02ebf6ac6bc7c0ddf0f4a98734993f068c21e526
Filippo Pellegrino <101466988+HipFil@users.noreply.github.com>
2022-05-30 14:42:23 +0200

    Update project_2.py

--- project_2.py
+++ project_2.py
@@ -27,6 +27,17 @@ endpoint = credential['Endpoint']
 
 cv_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(API_key))
 
+st.title('HW-Reader')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    image = Image.open('HW-REader.jpg')
+    st.image(image)
+with col2:
+    st.write('''Hello user! I am HW-Reader, a simple app to read, correct and translate your handwritten texts and notes.''')
+    st.write('''Before you start, let me briefly explain how I work: 1. select the language of your text file 2. upload a photo of your text 3. download the corrected text if needed 4. select the language and translate the text.''')
+
 source_lan = st.multiselect('give me a 2 letter word of your file langauge: ', ["BG",
         "CS",
         "DA",
@@ -54,48 +65,44 @@ source_lan = st.multiselect('give me a 2 letter word of your file langauge: ', [
         "TR",
         "ZH"])
 
+result1 = ""
+result2 = ""
 
-image_url = st.text_input('url: https://www.opensourceforu.com/wp-content/uploads/2016/09/Figure-1-Sample-Page-1.jpg')
-
-if image_url is not None:
-    #image = Image.open(image_url)
-    #st.image(image)
-    response = cv_client.read(url = image_url, Language= source_lan, raw=True)
+st.subheader("upload your image")
+st.write('''click on the following link: https://github.com/HipFil/Projectlesson3 and add your file through "add file" > "upload file". Once the image is displayed commit the file''')
+local_file = st.text_input('write "name_of_your_file.jpg"')
+if local_file != ' ':
+    
+    image = Image.open(local_file)
+    st.image(local_file)
+    
+    response = cv_client.read_in_stream(open(local_file, 'rb'), Language= source_lan, raw=True)
     operationLocation = response.headers['Operation-Location']
-    
     operation_id = operationLocation.split('/')[-1]
-    time.sleep(5)
-    
+    time.sleep(1)
     
     result = cv_client.get_read_result(operation_id)
-    #st.write(result)
-    st.write(result.status)
-    #st.write(result.analyze_result)
-    result = ""
-    result1 = ""
-    col1, col2 = st.columns(2)
-
+    
     if result.status == OperationStatusCodes.succeeded:
         read_results = result.analyze_result.read_results
+       
         for analyze_result in read_results:
             for line in analyze_result.lines:
                 line_text = line.text
-                st.write(line_text)
                 
-                #str_=re.findall("[a-zA-Z,.]+", line_text)
-                #updated_docx=(" ".join(str_))
-                #result = result + updated_docx
-                #new_doc = TextBlob(updated_docx)
-                #result1 = result1 + str(new_doc.correct())
-               
-    with col1:
-        st.subheader('the original text is:')
-        st.write(result)
-                    
-    with col2:
-        st.subheader('the corrected text is:')
-        st.write(result1)
-          
+                str_=re.findall("[a-zA-Z,.]+", line_text)
+                updated_docx=(" ".join(str_))
+                result1 = result1 + " " + updated_docx
+                new_doc = TextBlob(updated_docx)
+                result2 = result2 + str(new_doc.correct())
+                
+col1, col2 = st.columns(2)
+
+with col1:
+    st.write('the original text is:', result1)                
+with col2:
+    st.write('the corrected text is:', result2)
+
           
 choice = st.radio("'Do you want to translate the text?", ("yes", "no"))
 
